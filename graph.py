f5006325cb56825085007d0fba6e8e70aa606511
miguelgcoias <miguelgcoias@gmail.com>
2020-03-13 16:12:46 +0000

    improved graph data structure

--- graph.py
+++ graph.py
@@ -5,12 +5,28 @@ class Vertex:
 
     def __init__(self, v):
         self.v = v
+    
+    def __eq__(self, E):
+        return self.v == E.name()
+    
+    def __hash__(self):
+        return hash(self.v)
+
+    def name(self):
+        return self.v
+
 
 class Edge:
 
     def __init__(self, v, w):
         self.v = v
         self.w = w
+
+    def __eq__(self, E):
+        return self.v == E.start() and self.w == E.end()
+    
+    def __hash__(self):
+        return hash((self.v, self.w))
     
     def start(self):
         return self.v
@@ -18,31 +34,40 @@ class Edge:
     def end(self):
         return self.w
     
-    def merge(self, E):
-        if self.w == E.start():
-            return Edge(self.v, E.end())
-        elif self.v == E.end():
-            return Edge(E.start(), self.w)
-        else:
-            pass
+    def reverse(self):
+        self.v, self.w = self.w, self.v
+
 
 class Graph:
 
-    def __init__(self, V, E):
-        self.V = np.array([Vertex(v) for v in V])
-        self.E = dict()
+    # dir: whether the graph is directed or not. For now, does nothing
+    def __init__(self, V, E, dir=True):
+        self.G = dict.fromkeys([Vertex(v) for v in V], None)
+        self.dir = dir
 
-        for e in E:
-            self.E[Vertex(e[0])] = np.array([Edge(Vertex(e[0]), Vertex(v))
-            for v in e[1:]])
+        for v in E:
+            self.G[Vertex(v)] = [Edge(Vertex(v), Vertex(w)) for w in E[v]]
+    
+    def vertices(self):
+        return np.array([v for v in self.G.keys()])
+    
+    def edges(self):
+        # Not very good code, redo later
+        edge_list = self.G.copy()
+
+        for v in self.G:
+            if edge_list[v] is None:
+                edge_list.pop(v)
+        
+        return edge_list
         
     def neighbours(self, v):
-        return self.E[v]
-
-    def contract(self, nodes):
         pass
 
+    def contract(self, v, w):
+        pass
 
-# V = np.array(['A', 'B', 'C', 'D', 'E'])
-# E = np.array([['A', 'B', 'C'], ['B', 'C'], ['C', 'D'], ['D', 'E']])
-# G = Graph(V, E)
+V = ['A', 'B', 'C', 'D', 'E']
+E = {'A':['B', 'C'], 'B':['C'], 'C':['D'], 'D':['E']}
+G = Graph(V, E)
+print(G.edges())
