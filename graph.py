fbf74bdd35c70566684be56c4d9f362f83e83e64
miguelgcoias <miguelgcoias@gmail.com>
2020-03-16 21:24:40 +0000

    added support for JSON input, rebuilt graph data structure

--- graph.py
+++ graph.py
@@ -1,73 +1,24 @@
+import json
 import numpy as np
-
-
-class Vertex:
-
-    def __init__(self, v):
-        self.v = v
-    
-    def __eq__(self, E):
-        return self.v == E.name()
-    
-    def __hash__(self):
-        return hash(self.v)
-
-    def name(self):
-        return self.v
-
-
-class Edge:
-
-    def __init__(self, v, w):
-        self.v = v
-        self.w = w
-
-    def __eq__(self, E):
-        return self.v == E.start() and self.w == E.end()
-    
-    def __hash__(self):
-        return hash((self.v, self.w))
-    
-    def start(self):
-        return self.v
-
-    def end(self):
-        return self.w
-    
-    def reverse(self):
-        self.v, self.w = self.w, self.v
 
 
 class Graph:
 
-    # dir: whether the graph is directed or not. For now, does nothing
-    def __init__(self, V, E, dir=True):
-        self.G = dict.fromkeys([Vertex(v) for v in V], None)
-        self.dir = dir
+    def __init__(self, adj):
+        self.graph = self.__parse__(adj)
 
-        for v in E:
-            self.G[Vertex(v)] = [Edge(Vertex(v), Vertex(w)) for w in E[v]]
+    @staticmethod
+    def __parse__(adj):
+        with open(adj) as graph:
+            data = json.load(graph)
+            adj = np.empty(data['n'], dtype='O')
+            for pair in data['adj']:
+                adj[pair['v']] = np.array(pair['e'])
+        return adj
     
-    def vertices(self):
-        return np.array([v for v in self.G.keys()])
-    
-    def edges(self):
-        # Not very good code, redo later
-        edge_list = self.G.copy()
-
-        for v in self.G:
-            if edge_list[v] is None:
-                edge_list.pop(v)
-        
-        return edge_list
-        
     def neighbours(self, v):
-        pass
+        return self.graph[v]
 
-    def contract(self, v, w):
-        pass
 
-V = ['A', 'B', 'C', 'D', 'E']
-E = {'A':['B', 'C'], 'B':['C'], 'C':['D'], 'D':['E']}
-G = Graph(V, E)
-print(G.edges())
+# JSON file generated randomly -- not a directed graph!
+G = Graph('example.json')
