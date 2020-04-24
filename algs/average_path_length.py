77ee63a686816fc6968e2f7a5ea4c5bc7f010b31
João Tavares <jcostavares@gmail.com>
2020-04-24 14:50:40 +0100

    First version of average_path_length

---
+++ algs/average_path_length.py
@@ -0,0 +1,18 @@
+from algs.bellman_ford import bellman_ford
+
+def average_path_length(graph, algorithm='BFS'):
+    n = graph.order()
+
+    ret = 0
+    for v in range(n):
+        if algorithm == 'Bellman_Ford':
+            dist, _ = bellman_ford(graph, v)
+        elif algorithm == 'BFS':
+            dist, _, _ = BFS(graph, v)
+
+        for u in range(v+1, n):
+            ret += dist[u]
+    ret *= 2
+    ret /= n * (n-1)
+
+    return ret
