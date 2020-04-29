b3f861ddb87c873ea2ea6a6c8f9d7f3e6c3071bb
João Tavares <jcostavares@gmail.com>
2020-04-29 21:32:06 +0100

    Save plot

--- benchmark/utils.py
+++ benchmark/utils.py
@@ -4,3 +4,13 @@ def measure(func, x, n_runs=1, n_repeats=1):
     timer = Timer(lambda: func(x))
     times = timer.repeat(n_repeats, n_runs)
     return min(times)
+
+def plot(x, y, path=None):
+    import matplotlib.pyplot as plt
+    fig = plt.figure()
+    plt.plot(x, y)
+
+    if path:
+        fig.savefig(path)
+    else:
+        plt.show()
