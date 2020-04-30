692e6b2cf479be3017acc95e17583c01787cfb74
João Tavares <jcostavares@gmail.com>
2020-04-30 20:03:33 +0100

    changed script

--- run_tests.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import os
-
-for f in os.listdir('./benchmark'):
-    if f.endswith('.py'):
-        test_name = f[:-3]
-        print('Running test : ', test_name.upper())
-        os.system('python -m benchmark.' + test_name)
