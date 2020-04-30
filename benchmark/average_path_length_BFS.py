f7832c522db3adc1edc1ac8fdc1256c6f0b4a90b
João Tavares <jcostavares@gmail.com>
2020-04-30 20:04:12 +0100

    Added benchmark

---
+++ benchmark/average_path_length_BFS.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import numpy as np
+
+from structs.graph import Graph
+from benchmark.utils import plot, measure
+from algorithms.average_path_length import average_path_length
+
+# Set arguments
+args = {'ylabel': 'Runtime (s)',
+        'xlabel': 'V(V+E)'}
+
+# Find graphs to test
+graph_paths = []
+for g in os.listdir('./examples'):
+    if g.endswith('.json'):
+        graph_paths.append(g)
+
+num_graphs = len(graph_paths)
+sizes = np.empty(num_graphs, dtype=np.int64)
+times = np.empty(num_graphs, dtype=np.double)
+apl = lambda g: average_path_length(g, algorithm="BFS")
+n_runs, n_repeats = int(sys.argv[1]), int(sys.argv[2])
+
+# Run the algorithm
+for idx, g in enumerate(graph_paths):
+    print(g.upper())
+    graph = Graph('examples/' + g)
+    sizes[idx] = graph.order() * (graph.order() + graph.size())
+    times[idx] = measure(apl, graph, n_runs=n_runs, n_repeats=n_repeats)
+
+plot(sizes, times, args)
+print("OK")
--- benchmark/utils.py
+++ benchmark/utils.py
@@ -5,12 +5,18 @@ def measure(func, x, n_runs=1, n_repeats=1):
     times = timer.repeat(n_repeats, n_runs)
     return min(times)
 
-def plot(x, y, path=None):
+def plot(x, y, args={}):
     import matplotlib.pyplot as plt
     fig = plt.figure()
     plt.scatter(x, y)
 
-    if path:
-        fig.savefig(path)
+    if 'ylabel' in args:
+        plt.ylabel(args['ylabel'])
+
+    if 'xlabel' in args:
+        plt.xlabel(args['xlabel'])
+
+    if 'path' in args:
+        fig.savefig(args['path'])
     else:
         plt.show()
