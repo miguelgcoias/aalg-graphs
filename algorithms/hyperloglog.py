33aaf19a0fca27dad5c02ea825a299e50f20c718
João Tavares <jcostavares@gmail.com>
2020-05-05 22:59:33 +0100

    Implementation of HyperBall using HyperLogLogCounters

--- algorithms/hyperloglog.py
+++ algorithms/hyperloglog.py
@@ -1,43 +1,28 @@
 import numpy as np
 
-"""
-bits are indexed from 1 to nbits as follows
-x_31 x_30 ... x_1 x_0
-"""
-
-def leftmost_one(x, l, r):
-    for i in range(l, r-1, -1):
+def leading_zeros(x, left=32, right=0):
+    for i in range(left, right, -1):
         if x & 2 ** i != 0:
-            return i
-    return r-1
+            return (left-i) + 1
 
-def hyperloglog(b, h):
-    m = 2 ** b
-    r = np.zeros(m, dtype=np.uint32)
+    return (left-right) + 1
 
-    nbits = 32
-    for x in stream:
-        k = h(x)
-        j = leftmost_one(k, nbits-1, )
-        w = x & (2**(32-b) - 1)
-        r[j] = max(r[j], leftmost_one(w)-b)
+class HyperLogLogCounter:
+    def __init__(self, b):
+        self.b = b
+        self.p = 2 ** b
+        self.counter = np.zeros(self.p, dtype=np.uint8)
 
-    E = 0
-    for j in range(m):
-        E += 2 ** (-r[j])
+    def size(self):
+        ret = 0
+        for j in range(self.p):
+            ret += 2 ** (-self.counter[j])
 
-    E = alpha(m) * m**2 / E
+        ret = self.alpha * self.p**2 / ret
+        return ret
 
-    # Error Corrections
-    if 2*E <= 5*m:
-        V = 0
-        for i in range(m):
-            if r[i] == 0:
-                V + 1
-        if V != 0:
-            E = m*log(m / V)
+    def add(self, x):
+        pass
 
-    if 30*E > 2**32:
-        E = -2**32 * log(1 - E/2**32)
-
-    return E
+    def union(self, other):
+        pass
