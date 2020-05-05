5b0d531625cb6fe459f903c6a3ef8a68c39e140a
João Tavares <jcostavares@gmail.com>
2020-05-05 23:01:02 +0100

    Removed obsolete code

--- algorithms/average_path_length.py
+++ algorithms/average_path_length.py
@@ -7,9 +7,6 @@ def average_path_length(graph, algorithm='BFS'):
     n = graph.order()
 
     ret = 0
-    if algorithm == 'Flajolet_Martin':
-        ret = approximate_distance_sum(graph)
-        
     for v in range(n):
         if algorithm == 'Bellman_Ford':
             dist, _ = bellman_ford(graph, v)
@@ -23,36 +20,3 @@ def average_path_length(graph, algorithm='BFS'):
     ret /= n * (n-1)
 
     return ret
-
-def approximate_distance_sum(graph, hash_functions):
-    n = graph.order()
-    k = len(hash_functions)
-    m = np.int64(2 ** 32)
-    max_zeros = 32
-
-    r = np.zeros((n, k), dtype=np.int64)
-    neighbours = np.zeros(n, dtype=np.int64)
-
-    for edge in graph:
-        u, v = edge[:2]
-        for idx, h in enumerate(hash_functions):
-            r[u][idx] = max(r[u][idx], tail_length(h(v, m), max_zeros))
-            r[v][idx] = max(r[v][idx], tail_length(h(u, m), max_zeros))
-
-    for u in range(n):
-        tmp = np.double(0)
-        for idx in range(k):
-            tmp += r[u][idx]
-        tmp /= k
-
-        neighbours[u] = 2 ** tmp
-    return neighbours
-
-def tail_length(x, m):
-    if x == 0:
-        return m
-
-    n = np.int64(0)
-    while x % 2 ** (n+1) == 0:
-        n += 1
-    return n
