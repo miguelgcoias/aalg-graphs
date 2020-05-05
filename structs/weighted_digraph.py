e6ebdefa97b14b0bef7bc3c6035a825e33e9fdb9
miguelgcoias <miguelgcoias@gmail.com>
2020-05-05 19:08:11 +0100

    remade Graph and Digraph classes with weighted and unweighted variants

--- structs/weighted_digraph.py
+++ structs/weighted_digraph.py
@@ -2,32 +2,34 @@ from json import load
 
 import numpy as np
 
-from structs.graph import Graph
+from structs.weighted_graph import WeightedGraph
 
 
-class Digraph(Graph):
+class WeightedDigraph(WeightedGraph):
 
     def __init__(self, data):
-        '''Static sparse digraph class constructor. Allows reading from a JSON 
-        file or from a dictionary object mimicking the structure of a loaded 
-        JSON file.
+        '''Static sparse weighted digraph class constructor. Allows reading 
+        from a JSON file or from a dictionary object mimicking the structure of 
+        a loaded JSON file.
         
         Keyword arguments:
         data -- path to JSON file, or dictionary object as described above'''
         if isinstance(data, str):
             with open(data) as graph_json:
                 graph = load(graph_json)
-                self.n, self.m, self.adj, self.ind = super()._parse(graph)
+                self.n, self.m, self.adj, self.ind, self.weights = \
+                    super()._parse(graph)
         elif isinstance(data, dict):
-            self.n, self.m, self.adj, self.ind = super()._parse(data)
+            self.n, self.m, self.adj, self.ind, self.weights = \
+                super()._parse(data)
         else:
             raise RuntimeError('Invalid input')
 
     def __next__(self):
         if self.i < self.n:
-            pairs = self.neighbours(self.i)
+            pairs, weights = self.neighbours(self.i)
             if self.j < pairs.size:
-                edge = (self.i, pairs[self.j])
+                edge = (self.i, pairs[self.j], weights[self.j])
                 self.j += 1
                 return edge
             else:
--- structs/weighted_graph.py
+++ structs/weighted_graph.py
@@ -2,23 +2,27 @@ from json import load
 
 import numpy as np
 
+from structs.graph import Graph
 
-class Graph:
+
+class WeightedGraph(Graph):
 
     def __init__(self, data):
-        '''Static sparse unweighted graph class constructor. Allows reading 
-        from a JSON file or from a dictionary object mimicking the structure of 
-        a loaded JSON file.
+        '''Static sparse weighted graph class constructor. Allows reading from 
+        a JSON file or from a dictionary object mimicking the structure of a 
+        loaded JSON file.
         
         Keyword arguments:
         data -- path to JSON file, or dictionary object as described above'''
         if isinstance(data, str):
             with open(data) as graph_json:
                 graph = load(graph_json)
-                self.n, self.m, self.adj, self.ind = self._parse(graph)
+                self.n, self.m, self.adj, self.ind, self.weights = \
+                    self._parse(graph)
                 self.m = int(self.m/2)
         elif isinstance(data, dict):
-            self.n, self.m, self.adj, self.ind = self._parse(data)
+            self.n, self.m, self.adj, self.ind, self.weights = \
+                self._parse(data)
             self.m = int(self.m/2)
         else:
             raise RuntimeError('Invalid input')
@@ -26,11 +30,13 @@ class Graph:
 
     @staticmethod
     def _parse(graph):
-        adj = np.array([v for adj in graph.values() for v in adj],
+        adj = np.array([v for adj in graph.values() for v in adj[0]],
         dtype='u4')
-        ind = np.cumsum(np.array([0] + [len(adj) for adj in graph.values()]), 
+        weights = np.array([w for adj in graph.values() for w in adj[1]],
+        dtype='f8')
+        ind = np.cumsum(np.array([0] + [len(adj[0]) for adj in graph.values()]),
         dtype='u4')
-        return (ind.size - 1, adj.size, adj, ind)
+        return (ind.size - 1, adj.size, adj, ind, weights)
     
     def __iter__(self):
         self.i, self.j = 0, 0
@@ -38,10 +44,10 @@ class Graph:
 
     def __next__(self):
         if self.i < self.n:
-            pairs = self.neighbours(self.i)
+            pairs, weights = self.neighbours(self.i)
             if self.j < pairs.size:
                 if self.i <= pairs[self.j]:
-                    edge = (self.i, pairs[self.j])
+                    edge = (self.i, pairs[self.j], weights[self.j])
                     self.j += 1
                     return edge
                 else:
@@ -56,13 +62,5 @@ class Graph:
     
     def neighbours(self, v):
         '''Returns array of neighbours of vertex v.'''
-        endp = slice(self.ind[v], self.ind[v+1])
-        return self.adj[endp]
-
-    def order(self):
-        '''Return number of vertices.'''
-        return self.n
-
-    def size(self):
-        '''Return number of edges.'''
-        return self.m
+        inds = slice(self.ind[v], self.ind[v + 1])
+        return self.adj[inds], self.weights[inds]
