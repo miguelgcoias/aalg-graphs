d8c06fd6e32b1a3d87458dc3b3992655f0d50d19
João Tavares <jcostavares@gmail.com>
2020-05-21 00:02:06 +0100

    Removed old hyperloglog implementation

--- algorithms/hyperloglog.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def leading_zeros(x, left=32, right=0):
-    for i in range(left, right, -1):
-        if x & 2 ** i != 0:
-            return (left-i) + 1
-
-    return (left-right) + 1
